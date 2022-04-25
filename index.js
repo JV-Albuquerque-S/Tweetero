d0d53706c1894a97897153e88ca73c1c958143b0
JV-Albuquerque-S <alb.albqrq@gmail.com>
2022-04-25 15:44:09 -0300

    feat: funçao POST para tweets implementada

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const login = []
+const login = [];
+const posts = [];
 
 app.post("/sign-up", (req, res) => {
     const conta = req.body;
@@ -13,7 +14,17 @@ app.post("/sign-up", (req, res) => {
     res.send("OK");
 })
 
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    posts.push(tweet);
+    res.send("OK");
+})
 
+app.get("/tweets", (req, res) => {
+    let resposta = [];
+    
+    res.send(resposta);
+})
 
 app.listen(5000, () => {
     console.log("teste")
